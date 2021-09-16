429fc8e85c371e6043b0e2360108e9565af93fa3
jdcloud-apigateway <jdcloud-api@jd.com>
2021-09-16 08:47:29 +0000

    add updates

--- src/data/jke/v1/api-2.json.php
+++ src/data/jke/v1/api-2.json.php
@@ -22,41 +22,5 @@ return [
         ],
     ],
     'shapes' => [
-        'Quota' => [
-            'type' => 'structure',
-            'members' => [
-                'resourceType' => [ 'type' => 'string', 'locationName' => 'resourceType', ],
-                'limit' => [ 'type' => 'integer', 'locationName' => 'limit', ],
-                'used' => [ 'type' => 'integer', 'locationName' => 'used', ],
-            ],
-        ],
-        'Filter' => [
-            'type' => 'structure',
-            'members' => [
-                'name' => [ 'type' => 'string', 'locationName' => 'name', ],
-                'operator' => [ 'type' => 'string', 'locationName' => 'operator', ],
-                'values' => [ 'type' => 'list', 'member' => [ 'type' => 'string', ], ],
-            ],
-        ],
-        'DescribeQuotasResultShape' => [
-            'type' => 'structure',
-            'members' => [
-                'quotas' => [ 'type' => 'list', 'member' => [ 'shape' => 'Quota', ], ],
-            ],
-        ],
-        'DescribeQuotasResponseShape' => [
-            'type' => 'structure',
-            'members' => [
-                'result' =>  [ 'shape' => 'DescribeQuotasResultShape', ],
-                'requestId' => [ 'type' => 'string', 'locationName' => 'requestId', ],
-            ],
-        ],
-        'DescribeQuotasRequestShape' => [
-            'type' => 'structure',
-            'members' => [
-                'filters' => [ 'type' => 'list', 'member' => [ 'shape' => 'Filter', ], ],
-                'regionId' => [ 'type' => 'string', 'locationName' => 'regionId', ],
-            ],
-        ],
     ],
 ];
